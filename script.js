0260160e6a509832bf1b239a1a478da39f0b02aa
vishalkumarkhatri <vk.khatri2003@gmail.com>
2025-06-07 21:59:56 +0530

    modify script file

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const windValueTxt = document.querySelector(".wind-speed-value");
 
 const forecastItemContainer = document.querySelector(".forecast-item-container");
 
-const apiKey = "99d40f00bf52f0a38acca9cc15e79a71";
+const apiKey = "";
 
 searchBtn.addEventListener("click", () => {
     if (cityInput.ariaValueMax.trim() != "") {
